d7dc7fd9c6d3f6b51a4b83aa7bbf41a91659dc95
Galinouski <autodjam@yandex.ru>
2024-07-08 11:56:25 +0300

    Add files via upload
    1

---
+++ templates/download.php
@@ -0,0 +1,39 @@
+<?php
+global $base_path;
+require_once $base_path . 'library\core.php';
+$errors = [];
+
+if(isset($_GET['path']) && str_contains($_GET['path'], 'downloads'))
+{
+    $url = $_GET['path'];
+    // clear cash
+    clearstatcache();
+
+    if(file_exists($url)) {
+
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.basename($url).'"');
+        header('Content-Length: '. filesize($url));
+        header('Pragma: public');
+
+        // clear buffer
+        flush();
+
+        readfile($url,true);
+
+        die();
+    }
+    else{
+        $errors [] = "The file you tried to download does not exist. ";
+    }
+}
+$errors [] = "This file does not exist.";
+
+if ($errors) {
+
+    $errors [] = "<a href='index.php?path=error'>back</a>";
+    $context = ['errors'=>$errors];
+    render('main', $context);
+    exit();
+}
