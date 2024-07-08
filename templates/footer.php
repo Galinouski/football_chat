e5b1bed50755bd8e2c012f70605facd00d99f7a7
Galinouski <autodjam@yandex.ru>
2024-07-08 11:19:36 +0300

    Add files via upload
    11.20

---
+++ templates/footer.php
@@ -0,0 +1,7 @@
+<div class="container fluid">
+    <div class="row no-gutters justify-content-start">
+        <div class="col" style="margin-top: 50px">
+            <img class="image" src="images/2.jpg">
+        </div>
+    </div>
+</div>
